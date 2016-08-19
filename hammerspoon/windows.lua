06a6be505d932a5be4d9a7e1af800a8940c7c400
Jason Wang <jwang@admins-MacBook-Pro-3.local>
2016-08-19 16:15:46 -0700

    `hammerspoon` - cleaned up `windows.lua`

--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -1,100 +1,113 @@
+local function fullScreen()
+  local win = hs.window.focusedWindow()
+  local frame = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  frame.x = max.x
+  frame.y = max.y
+  frame.w = max.w
+  frame.h = max.h
+  win:setFrame(frame)
+end
+
+local function center()
+  local win = hs.window.focusedWindow()
+
+  win:centerOnScreen()
+end
+
+local function leftHalf()
+  local win = hs.window.focusedWindow()
+  local frame = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  frame.x = max.x
+  frame.y = max.y
+  frame.w = max.w / 2
+  frame.h = max.h
+  win:setFrame(frame)
+end
+
+local function bottomHalf()
+  local win = hs.window.focusedWindow()
+  local frame = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  frame.x = max.x
+  frame.y = max.y + max.h / 2
+  frame.w = max.w
+  frame.h = max.h / 2
+  win:setFrame(frame)
+end
+
+local function topHalf()
+  local win = hs.window.focusedWindow()
+  local frame = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  frame.x = max.x
+  frame.y = max.y
+  frame.w = max.w
+  frame.h = max.h / 2
+  win:setFrame(frame)
+end
+
+local function rightHalf()
+  local win = hs.window.focusedWindow()
+  local frame = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  frame.x = max.x + max.w / 2
+  frame.y = max.y
+  frame.w = max.w / 2
+  frame.h = max.h
+  win:setFrame(frame)
+end
+
+local function leftScreen()
+  local win = hs.window.focusedWindow()
+
+  win:moveOneScreenWest(true, true)
+end
+
+local function lowerScreen()
+  local win = hs.window.focusedWindow()
+
+  win:moveOneScreenSouth(true, true)
+end
+
+local function upperScreen()
+  local win = hs.window.focusedWindow()
+
+  win:moveOneScreenNorth(true, true)
+end
+
+local function rightScreen()
+  local win = hs.window.focusedWindow()
+
+  win:moveOneScreenEast(true, true)
+end
+
 local function windows(hyper, hyperControl)
   hs.window.animationDuration = 0
 
-  hs.hotkey.bind(hyper, 'f', function ()
-    local win = hs.window.focusedWindow()
-    local frame = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
+  hs.hotkey.bind(hyper, 'f', fullScreen)
+  hs.hotkey.bind(hyper, 'c', center)
+  
+  hs.hotkey.bind(hyper, 'h', leftHalf)
+  hs.hotkey.bind(hyper, 'j', bottomHalf)
+  hs.hotkey.bind(hyper, 'k', topHalf)
+  hs.hotkey.bind(hyper, 'l', rightHalf)
 
-    frame.x = max.x
-    frame.y = max.y
-    frame.w = max.w
-    frame.h = max.h
-    win:setFrame(frame)
-  end)
-
-  hs.hotkey.bind(hyper, 'c', function ()
-    local win = hs.window.focusedWindow()
-
-    win:centerOnScreen()
-  end)
-
-  hs.hotkey.bind(hyper, 'h', function ()
-    local win = hs.window.focusedWindow()
-    local frame = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    frame.x = max.x
-    frame.y = max.y
-    frame.w = max.w / 2
-    frame.h = max.h
-    win:setFrame(frame)
-  end)
-
-  hs.hotkey.bind(hyper, 'j', function ()
-    local win = hs.window.focusedWindow()
-    local frame = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    frame.x = max.x
-    frame.y = max.y + max.h / 2
-    frame.w = max.w
-    frame.h = max.h / 2
-    win:setFrame(frame)
-  end)
-
-  hs.hotkey.bind(hyper, 'k', function ()
-    local win = hs.window.focusedWindow()
-    local frame = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    frame.x = max.x
-    frame.y = max.y
-    frame.w = max.w
-    frame.h = max.h / 2
-    win:setFrame(frame)
-  end)
-
-  hs.hotkey.bind(hyper, 'l', function ()
-    local win = hs.window.focusedWindow()
-    local frame = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    frame.x = max.x + max.w / 2
-    frame.y = max.y
-    frame.w = max.w / 2
-    frame.h = max.h
-    win:setFrame(frame)
-  end)
-
-  hs.hotkey.bind(hyperControl, 'h', function ()
-    local win = hs.window.focusedWindow()
-
-    win:moveOneScreenWest(true, true)
-  end)
-
-  hs.hotkey.bind(hyperControl, 'j', function ()
-    local win = hs.window.focusedWindow()
-
-    win:moveOneScreenSouth(true, true)
-  end)
-
-  hs.hotkey.bind(hyperControl, 'k', function ()
-    local win = hs.window.focusedWindow()
-
-    win:moveOneScreenNorth(true, true)
-  end)
-
-  hs.hotkey.bind(hyperControl, 'l', function ()
-    local win = hs.window.focusedWindow()
-
-    win:moveOneScreenEast(true, true)
-  end)
+  hs.hotkey.bind(hyperControl, 'h', leftScreen)
+  hs.hotkey.bind(hyperControl, 'j', lowerScreen)
+  hs.hotkey.bind(hyperControl, 'k', upperScreen)
+  hs.hotkey.bind(hyperControl, 'l', rightScreen)
 end
 
 return windows
