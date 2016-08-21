34d5531a04c48e37c0e9f5d0c1b8d3b27ee45ace
Jason Wang <jasonwang@Jasons-MacBook-Pro-2.local>
2016-08-21 02:16:35 -0700

    hammerspoon - Added some TODOs

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,3 +4,6 @@ local hyperControl = { 'cmd', 'alt', 'ctrl' }
 require('reloadConfig')(hyper, hyperControl)
 require('applications')(hyper, hyperControl)
 require('windows')(hyper, hyperControl)
+
+-- TODO: Bind hyper, '.' to open dotfiles in Atom
+-- TODO: Improve LastPass workflow
