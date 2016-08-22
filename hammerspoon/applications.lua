4960f53f0a18d6a253c5b2b1d4b9fdeaa83add0e
Jason Wang <jasonwang@Jasons-MacBook-Pro-2.local>
2016-08-21 21:23:26 -0700

    hammerspoon - Added `escape` key to exit application mode

--- hammerspoon/applications.lua
+++ hammerspoon/applications.lua
@@ -9,11 +9,12 @@ local function applications(hyper)
     end
   end
 
-  applicationModal:bind(hyper, 'a', nil, function () launch('Atom') end)
-  applicationModal:bind(hyper, 'c', nil, function () launch('Google Chrome') end)
-  applicationModal:bind(hyper, 'm', nil, function () launch('Messages') end)
-  applicationModal:bind(hyper, 's', nil, function () launch('Slack') end)
-  applicationModal:bind(hyper, 't', nil, function () launch('Terminal') end)
+  applicationModal:bind(hyper, 'a', function () launch('Atom') end)
+  applicationModal:bind(hyper, 'c', function () launch('Google Chrome') end)
+  applicationModal:bind(hyper, 'm', function () launch('Messages') end)
+  applicationModal:bind(hyper, 's', function () launch('Slack') end)
+  applicationModal:bind(hyper, 't', function () launch('Terminal') end)
+  applicationModal:bind('', 'escape', function () applicationModal:exit() end)
 end
 
 return applications
