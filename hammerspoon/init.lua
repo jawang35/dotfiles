558a04b1f3f9e4978bd42fa8235a7cf21b3740ae
Jason Wang <jwang@unknownb6750e179426.gateway.pace.com>
2016-08-22 17:08:28 -0700

    hammerspoon - Added some Spotify controls

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,7 +2,9 @@ local hyper = { 'cmd', 'alt', 'ctrl' }
 local hyperShift = { 'cmd', 'alt', 'ctrl', 'shift' }
 
 require('reloadConfig')(hyper, hyperShift)
+
 require('applications')(hyper, hyperShift)
+require('spotify')(hyper, hyperShift)
 require('windows')(hyper, hyperShift)
 
 -- TODO: Bind hyper, '.' to open dotfiles in Atom
---
+++ hammerspoon/spotify.lua
@@ -0,0 +1,62 @@
+local spotifyIcon = hs.image.imageFromPath('resources/spotify.png')
+
+local function spotifyNotification(title, text)
+  hs.notify.new({ title = title, informativeText = text }):setIdImage(spotifyIcon):send()
+end
+
+local function getCurrentSong()
+  return 'Artist: ' .. hs.spotify.getCurrentArtist() .. '\nSong: ' .. hs.spotify.getCurrentTrack()
+end
+
+local function open()
+  if not hs.application.launchOrFocus('Spotify') then
+    hs.alert.show('Spotify not found')
+  end
+end
+
+local function previous()
+  hs.spotify.previous()
+  spotifyNotification('⏮ Previous', getCurrentSong())
+end
+
+local function next()
+  hs.spotify.next()
+  spotifyNotification('⏭ Next', getCurrentSong())
+end
+
+local function play()
+  hs.spotify.play()
+  spotifyNotification('▶️ Play', getCurrentSong())
+end
+
+local function pause()
+  hs.spotify.pause()
+  spotifyNotification('⏸ Pause', getCurrentSong())
+end
+
+local function spotify(hyper)
+  local spotifyModal = hs.hotkey.modal.new(hyper, 's')
+  local menubar = hs.menubar.new()
+
+  function spotifyModal:entered()
+    menubar:setTitle('Spotify Mode')
+  end
+
+  function spotifyModal:exited()
+    menubar:setTitle(nil)
+  end
+
+  local function execute(callback)
+    spotifyModal:exit()
+    callback()
+  end
+
+  spotifyModal:bind('', 'escape', function () spotifyModal:exit() end)
+  spotifyModal:bind(hyper, 's', function () execute(open) end)
+  spotifyModal:bind(hyper, 'l', function () execute(next) end)
+  spotifyModal:bind(hyper, 'h', function () execute(previous) end)
+  spotifyModal:bind(hyper, 'j', function () execute(pause) end)
+  spotifyModal:bind(hyper, 'k', function () execute(play) end)
+end
+
+return spotify
