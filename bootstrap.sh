5065e762bc3bd178215bc30891a55915a5028ad7
jawang35 <jwang@surfline.com>
2019-08-28 22:15:30 -0700

    Simplify bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/env bash
-.
-..
-.DS_Store
-.bash_profile
-.bashrc
-.editorconfig
-.git
-.gitignore
-.gitmodules
-.inputrc
-.profile
-.psqlrc
-.shell_prompt.sh
-.tmux.conf
-LICENSE
-README.md
-atom
-bootstrap.sh
-hammerspoon
-karabiner
-vim
 
 set -xe
 
@@ -39,14 +18,12 @@ sync () {
 
 git submodule update --init --recursive
 
-sync ./ "${HOME}/"
-sync atom/ "${HOME}/.atom/"
-sync hammerspoon/ "${HOME}/.hammerspoon/"
-sync karabiner/ "${HOME}/.config/karabiner/"
-
-# Vim
-if command -v nvim >/dev/null 2>&1; then
-    sync vim/ "${HOME}/.config/nvim/"
-else
-    sync vim/ "${HOME}/.vim/"
-fi
+rsync -avh \
+    --no-perms \
+    --exclude .git/ \
+    --exclude .gitignore \
+    --exclude .gitmodules \
+    --exclude LICENSE \
+    --exclude README.md \
+    --exclude bootstrap.sh \
+    . ~
