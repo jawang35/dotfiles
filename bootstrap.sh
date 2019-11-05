751eb43f398f9fd881cd3a24adfdd4ee4621c81e
jawang35 <jawang35@gmail.com>
2019-11-05 01:46:52 -0800

    iterm2 preferences

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-source ${HOME}/.bash_profile
 git --git-dir=${HOME}/.dotfiles.git --work-tree=${HOME} submodule update --init --recursive
 if command -v brew >/dev/null 2>&1; then
     brew bundle
 fi
+
+source ${HOME}/.bash_profile
