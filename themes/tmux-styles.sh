df2a09a342daadfc66a379f50e8cf0c6f04148dc
jawang35 <jawang35@gmail.com>
2019-12-01 22:31:15 -0800

    Improve onehalf-inspired colors

--- themes/tmux-styles.sh
+++ themes/tmux-styles.sh
@@ -29,7 +29,7 @@ tmux set -g status-left-length '100'
 tmux setw -g window-status-activity-style "none"
 tmux setw -g window-status-separator ''
 tmux setw -g window-status-style "none,fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_DARK_GRAY"
-tmux set -g status-left "#[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #S #[fg=$TMUX_COLOR_PURPLE,bg=$TMUX_COLOR_DARK_GRAY,nobold,nounderscore,noitalics]"
-tmux set -g status-right "#[fg=$TMUX_COLOR_LIGHT_GRAY,bg=$TMUX_COLOR_DARK_GRAY,nobold,nounderscore,noitalics]#[fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_LIGHT_GRAY] %Y-%m-%d | %H:%M #[fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_LIGHT_GRAY,nobold,nounderscore,noitalics]#[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #h "
+tmux set -g status-left "#[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #S "
+tmux set -g status-right "#[fg=$TMUX_COLOR_PURPLE,bg=$TMUX_COLOR_DARK_GRAY] %Y-%m-%d | %H:%M #[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #h "
 tmux setw -g window-status-format "#[fg=$TMUX_COLOR_LIGHT_GRAY,bg=$TMUX_COLOR_DARK_GRAY] #I:#W "
-tmux setw -g window-status-current-format "#[fg=$TMUX_COLOR_DARK_GRAY,bg=$TMUX_COLOR_LIGHT_GRAY,nobold,nounderscore,noitalics]#[fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_LIGHT_GRAY] #I:#W "
+tmux setw -g window-status-current-format "#[fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_LIGHT_GRAY] #I:#W "
