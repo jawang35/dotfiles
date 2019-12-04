0ed91b34da53e43c5ec269edad1c4743b9d9f1a9
jawang35 <jwang@surfline.com>
2019-12-04 10:15:34 -0800

    Fix brew installed completions with backwards compatibility

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@ git clone --bare https://github.com/jawang35/.dotfiles.git "${HOME}/.dotfiles.gi
 git --git-dir="${HOME}/.dotfiles.git" --work-tree="${HOME}" checkout
 git --git-dir="${HOME}/.dotfiles.git" --work-tree="${HOME}" submodule update --init --recursive
 
+if command -v brew >/dev/null 2>&1; then
+    brew update && brew bundle --file="${HOME}/.config/Brewfile"
+fi
+
 # shellcheck source=.bash_profile
 source "${HOME}/.bash_profile"
-
-if command -v brew >/dev/null 2>&1; then
-    brew update && brew bundle
-fi
