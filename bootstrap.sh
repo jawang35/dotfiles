4730cd531c378a5334010cf66b7d4a8aaa357a77
jawang35 <jawang35@gmail.com>
2019-12-03 21:34:11 -0800

    Source .bash_profile before brew bundle

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,10 @@ set -e
 git clone --bare https://github.com/jawang35/.dotfiles.git "${HOME}/.dotfiles.git"
 git --git-dir="${HOME}/.dotfiles.git" --work-tree="${HOME}" checkout
 git --git-dir="${HOME}/.dotfiles.git" --work-tree="${HOME}" submodule update --init --recursive
-if command -v brew >/dev/null 2>&1; then
-    brew update && brew bundle
-fi
 
 # shellcheck source=.bash_profile
 source "${HOME}/.bash_profile"
+
+if command -v brew >/dev/null 2>&1; then
+    brew update && brew bundle
+fi
