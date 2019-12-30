850d55d0d1e312bd0aa66def8545401328562649
Jason Wang <jawang35@gmail.com>
2019-12-29 23:01:48 -0800

    Colorize tmux cpu/mem

---
+++ .config/tmux/mem-cpu.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import argparse
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('colors', type=str, nargs=3)
+    colors = parser.parse_args().colors
+
+    command = 'tmux-mem-cpu-load -i1 -g0 -m2 -t0'
+    stats = os.popen(command).readlines()[0].split()
+    mem = float(stats[0][:-1])
+    cpu = float(stats[1][:-1])
+
+    if mem < 30:
+        mem_color = colors[0]
+    elif mem < 80:
+        mem_color = colors[1]
+    else:
+        mem_color = colors[2]
+
+    if cpu < 30:
+        cpu_color = colors[0]
+    elif cpu < 80:
+        cpu_color = colors[1]
+    else:
+        cpu_color = colors[2]
+
+    print(f'#[fg={mem_color}]M:{mem:.1f}%#[default] '
+          f'#[fg={cpu_color}]C:{cpu:.1f}%#[default]')
+
+
+if __name__ == '__main__':
+    main()
--- .config/tmux/status-bar.sh
+++ .config/tmux/status-bar.sh
@@ -8,12 +8,18 @@ if [ "$COLORTERM" = truecolor ] || [ "$COLORTERM" = 24bit ]; then
     TMUX_COLOR_LIGHT_GRAY='#5d677a'
     TMUX_COLOR_DARK_GRAY='#313640'
     TMUX_COLOR_PURPLE='#c678dd'
+    TMUX_COLOR_GREEN='#98c379'
+    TMUX_COLOR_YELLOW='#e5c07b'
+    TMUX_COLOR_RED='#e06c75'
 else
     TMUX_COLOR_WHITE='colour255'
     TMUX_COLOR_LIGHT_GRAY='colour243'
     TMUX_COLOR_DARK_GRAY='colour238'
     TMUX_COLOR_BLACK='colour236'
     TMUX_COLOR_PURPLE='colour176'
+    TMUX_COLOR_GREEN='colour114'
+    TMUX_COLOR_YELLOW='colour180'
+    TMUX_COLOR_RED='colour168'
 fi
 
 tmux set -g status-justify 'left'
@@ -33,6 +39,6 @@ tmux setw -g window-status-activity-style "none"
 tmux setw -g window-status-separator ''
 tmux setw -g window-status-style "none,fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_DARK_GRAY"
 tmux set -g status-left "#[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #S "
-tmux set -g status-right "#[fg=$TMUX_COLOR_PURPLE,bg=$TMUX_COLOR_DARK_GRAY]#(tmux-mem-cpu-load -i1 -g0 -m2 -t0 | awk '{print \"M:\"\$1, \"C:\"\$2}')#[default] #[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #h "
+tmux set -g status-right "#($HOME/.config/tmux/mem-cpu.py '$TMUX_COLOR_GREEN' '$TMUX_COLOR_YELLOW' '$TMUX_COLOR_RED')#[default] #[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #h "
 tmux setw -g window-status-format "#[fg=$TMUX_COLOR_LIGHT_GRAY,bg=$TMUX_COLOR_DARK_GRAY] #I:#W "
 tmux setw -g window-status-current-format "#[fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_LIGHT_GRAY] #I:#W "
