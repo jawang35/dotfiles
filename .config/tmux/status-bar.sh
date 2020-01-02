6a56c411a529e65504c075d3226b57171f85793f
Jason Wang <jawang35@gmail.com>
2020-01-02 14:12:32 -0800

    Highlight zoomed window in tmux

--- .config/tmux/status-bar.sh
+++ .config/tmux/status-bar.sh
@@ -2,7 +2,7 @@
 
 # tmux status bar based on the onehalfdark theme.
 
-if [ "$COLORTERM" = truecolor ] || [ "$COLORTERM" = 24bit ]; then
+if [ "${COLORTERM}" = truecolor ] || [ "${COLORTERM}" = 24bit ]; then
     TMUX_COLOR_WHITE='#dcdfe4'
     TMUX_COLOR_BLACK='#282c34'
     TMUX_COLOR_LIGHT_GRAY='#5d677a'
@@ -26,19 +26,19 @@ tmux set -g status-justify 'centre'
 tmux set -g status 'on'
 tmux set -g status-left-style 'none'
 tmux set -g status-interval '1'
-tmux set -g message-command-style "fg=$TMUX_COLOR_WHITE"
-tmux set -g message-command-style "bg=$TMUX_COLOR_LIGHT_GRAY"
+tmux set -g message-command-style "fg=${TMUX_COLOR_WHITE}"
+tmux set -g message-command-style "bg=${TMUX_COLOR_LIGHT_GRAY}"
 tmux set -g status-right-style 'none'
-tmux set -g pane-active-border-style "fg=$TMUX_COLOR_PURPLE"
-tmux set -g status-style "none,bg=$TMUX_COLOR_DARK_GRAY"
-tmux set -g message-style "fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_LIGHT_GRAY"
-tmux set -g pane-border-style "fg=$TMUX_COLOR_LIGHT_GRAY"
+tmux set -g pane-active-border-style "fg=${TMUX_COLOR_PURPLE}"
+tmux set -g status-style "none,bg=${TMUX_COLOR_DARK_GRAY}"
+tmux set -g message-style "fg=${TMUX_COLOR_WHITE},bg=${TMUX_COLOR_LIGHT_GRAY}"
+tmux set -g pane-border-style "fg=${TMUX_COLOR_LIGHT_GRAY}"
 tmux set -g status-right-length '100'
 tmux set -g status-left-length '100'
 tmux setw -g window-status-activity-style "none"
 tmux setw -g window-status-separator ''
-tmux setw -g window-status-style "none,fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_DARK_GRAY"
-tmux set -g status-left "#[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #S #[default] #($HOME/.config/tmux/mem-cpu.py '$TMUX_COLOR_GREEN' '$TMUX_COLOR_YELLOW' '$TMUX_COLOR_RED')"
-tmux set -g status-right "#[fg=$TMUX_COLOR_PURPLE]  %b %d  %H:%M #[fg=$TMUX_COLOR_BLACK,bg=$TMUX_COLOR_PURPLE] #h "
-tmux setw -g window-status-format "#[fg=$TMUX_COLOR_LIGHT_GRAY] #I:#W "
-tmux setw -g window-status-current-format "#[fg=$TMUX_COLOR_WHITE,bg=$TMUX_COLOR_LIGHT_GRAY] #I:#W "
+tmux setw -g window-status-style "none,fg=${TMUX_COLOR_WHITE},bg=${TMUX_COLOR_DARK_GRAY}"
+tmux set -g status-left "#[fg=${TMUX_COLOR_BLACK},bg=${TMUX_COLOR_PURPLE}] #S #[default] #(${HOME}/.config/tmux/mem-cpu.py '${TMUX_COLOR_GREEN}' '${TMUX_COLOR_YELLOW}' '${TMUX_COLOR_RED}')"
+tmux set -g status-right "#[fg=${TMUX_COLOR_PURPLE}]  %b %d  %H:%M #[fg=${TMUX_COLOR_BLACK},bg=${TMUX_COLOR_PURPLE}] #h "
+tmux setw -g window-status-format "#[fg=${TMUX_COLOR_LIGHT_GRAY}] #I:#W "
+tmux setw -g window-status-current-format "#[fg=#{?window_zoomed_flag,${TMUX_COLOR_DARK_GRAY},${TMUX_COLOR_WHITE}},bg=#{?window_zoomed_flag,${TMUX_COLOR_GREEN},${TMUX_COLOR_LIGHT_GRAY}}] #I:#W "
