74e811dcff3336a4e697182fad5079cc5939838a
Jason Wang <jawang35@gmail.com>
2020-01-23 09:50:57 -0800

    tmux status icons (remove date/time)

--- .config/tmux/status-bar.sh
+++ .config/tmux/status-bar.sh
@@ -8,21 +8,15 @@ if [ "${COLORTERM}" = truecolor ] || [ "${COLORTERM}" = 24bit ]; then
     TMUX_COLOR_DARK_GRAY='#313640'
     TMUX_COLOR_PURPLE='#c678dd'
     TMUX_COLOR_GREEN='#98c379'
-    TMUX_COLOR_YELLOW='#e5c07b'
-    TMUX_COLOR_RED='#e06c75'
-    TMUX_COLOR_BLUE='#61afef'
 else
     TMUX_COLOR_WHITE='colour255'
     TMUX_COLOR_LIGHT_GRAY='colour243'
     TMUX_COLOR_DARK_GRAY='colour238'
     TMUX_COLOR_PURPLE='colour176'
     TMUX_COLOR_GREEN='colour114'
-    TMUX_COLOR_YELLOW='colour180'
-    TMUX_COLOR_RED='colour168'
-    TMUX_COLOR_BLUE='colour75'
 fi
 
-TMUX_STATUS_COLOR="#{?client_prefix,${TMUX_COLOR_BLUE},${TMUX_COLOR_PURPLE}}"
+TMUX_STATUS_COLOR="${TMUX_COLOR_PURPLE}"
 
 tmux set -g status-justify 'centre'
 tmux set -g status 'on'
@@ -40,7 +34,7 @@ tmux set -g status-left-length '100'
 tmux setw -g window-status-activity-style "none"
 tmux setw -g window-status-separator ''
 tmux setw -g window-status-style "none,fg=${TMUX_COLOR_WHITE},bg=${TMUX_COLOR_DARK_GRAY}"
-tmux set -g status-left "#[fg=${TMUX_COLOR_DARK_GRAY},bg=${TMUX_STATUS_COLOR}] #S #[default] #(${HOME}/.config/tmux/mem-cpu.py '${TMUX_COLOR_GREEN}' '${TMUX_COLOR_YELLOW}' '${TMUX_COLOR_RED}')"
-tmux set -g status-right "#[fg=${TMUX_STATUS_COLOR}]  %b %d  %H:%M #[fg=${TMUX_COLOR_DARK_GRAY},bg=${TMUX_STATUS_COLOR}] #h "
+tmux set -g status-left "#[fg=${TMUX_COLOR_DARK_GRAY},bg=${TMUX_STATUS_COLOR}] #S #[default]#[fg=${TMUX_STATUS_COLOR}] #{?client_prefix,, } #{?window_zoomed_flag,, }"
+tmux set -g status-right "#[fg=${TMUX_COLOR_DARK_GRAY},bg=${TMUX_STATUS_COLOR}] #h "
 tmux setw -g window-status-format "#[fg=${TMUX_COLOR_LIGHT_GRAY}] #I:#W "
 tmux setw -g window-status-current-format "#[fg=#{?window_zoomed_flag,${TMUX_COLOR_DARK_GRAY},${TMUX_COLOR_WHITE}},bg=#{?window_zoomed_flag,${TMUX_COLOR_GREEN},${TMUX_COLOR_LIGHT_GRAY}}] #I:#W "
