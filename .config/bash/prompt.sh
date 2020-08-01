f28553360ec1f7a46092b27c2df7453c0d857409
Jason Wang <jawang35@gmail.com>
2020-08-01 00:40:28 -0700

    Temporarily check for starship in PROMPT_COMMAND

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 __bash_prompt() {
-    if command -v starship > /dev/null 2>&1; then
+    # TODO: Remove PROMPT_COMMAND check when https://github.com/starship/starship/pull/1541 is fixed.
+    if [[ "${PROMPT_COMMAND}" != *"starship_precmd"* ]] && command -v starship > /dev/null 2>&1; then
         eval "$(starship init bash)"
     fi
 }
