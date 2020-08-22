090900ade1a8b4564b2cc3c025e30d4f1688ea56
Jason Wang <jawang35@gmail.com>
2020-08-21 23:40:08 -0700

    Refactor gopen to handle no file and bitbucket

--- .config/bash/functions.sh
+++ .config/bash/functions.sh
@@ -48,31 +48,40 @@ __bash_functions() {
         git push --set-upstream "$(git remote show)" "$(git rev-parse --abbrev-ref HEAD)"
     }
 
+    # open git repository file in web browser
     gopen() {
-        local REMOTE
-        local FILE_PATH
+        local GIT_REPO
+        local GIT_BRANCH
+        local FILE_PATH="${1}"
+        local URL
 
-        if [ "${#}" -eq 1 ]; then
-            REMOTE=origin
-            FILE_PATH="${1}"
-        elif [ "${#}" -eq 2 ]; then
-            REMOTE="${1}"
-            FILE_PATH="${2}"
-        else
-            echo 'Path to file required.'
-            return 1
-        fi
-
+        GIT_BRANCH=$(git branch --show-current)
         GIT_REPO=$( \
             git remote -v | \
             grep "${REMOTE}" | \
             awk '{print $2}' | \
-            sort -u | \
+            head -n1 | \
             sed -e 's/\.git//' \
+                -e 's/git@bitbucket\.org:/https:\/\/bitbucket.org\//' \
+                -e 's/https:\/\/.*bitbucket\.org:/https:\/\/bitbucket.org\//' \
                 -e 's/git@github\.com:/https:\/\/github.com\//' \
                 -e 's/https:\/\/github\.com:/https:\/\/github.com\//' \
         )
-        python -m webbrowser -t "${GIT_REPO}/blob/master/${FILE_PATH}"
+
+        case "${GIT_REPO}" in
+            *bitbucket*)
+                URL="${GIT_REPO}/src/${GIT_BRANCH}/${FILE_PATH}"
+                ;;
+            *github*)
+                if [ -z "${FILE_PATH}" ]; then
+                    URL="${GIT_REPO}/tree/${GIT_BRANCH}"
+                else
+                    URL="${GIT_REPO}/blob/${GIT_BRANCH}/${FILE_PATH}"
+                fi
+                ;;
+        esac
+
+        python -m webbrowser -t "${URL}"
     }
 }
 
