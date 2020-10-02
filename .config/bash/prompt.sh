bfff8b8a8cf1db2cfe1fb97aa7ddf98032ed1113
Jason Wang <jawang35@gmail.com>
2020-10-01 17:17:14 -0700

    Remove PROMPT_COMMAND check for starship (https://github.com/starship/starship/pull/1541)

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 __bash_prompt() {
-    # TODO: Remove PROMPT_COMMAND check when https://github.com/starship/starship/pull/1541 is fixed.
-    if [[ "${PROMPT_COMMAND}" != *"starship_precmd"* ]] && command -v starship > /dev/null 2>&1; then
-        eval "$(starship init bash)"
-    fi
+    eval "$(starship init bash)"
 }
 
 __bash_prompt
