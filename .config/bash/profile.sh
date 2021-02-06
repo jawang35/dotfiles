28943c7a80d045c5c049c729e98bcc1b96e8c507
Jason Wang <jawang35@gmail.com>
2021-02-06 09:53:39 -0800

    fzf onedark color scheme

--- .config/bash/profile.sh
+++ .config/bash/profile.sh
@@ -50,7 +50,12 @@ __bash_profile() {
 
         export FZF_COMPLETION_TRIGGER='**'
         export FZF_DEFAULT_COMMAND='fd --exclude .git --hidden --type f'
-        export FZF_DEFAULT_OPTS="--height 40% --prompt='${PROMPT_SYMBOL} '"
+        export FZF_DEFAULT_OPTS="
+        --height 40%
+        --prompt='${PROMPT_SYMBOL} '
+        --color=dark
+        --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe
+        --color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef"
         export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
         export FZF_CTRL_T_OPTS="--preview='bat {} | head -500'"
         export FZF_CTRL_R_OPTS="--reverse"
