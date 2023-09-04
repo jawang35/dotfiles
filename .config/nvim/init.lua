87d5d0ebcf2a537348f170e5d8863b20c6f7b907
Jason Wang <jawang35@gmail.com>
2023-09-03 21:32:21 -0700

    Tab completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -66,7 +66,20 @@ if vim.loop.fs_stat(lazypath) then
         'neovim/nvim-lspconfig',
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
-        'hrsh7th/nvim-cmp',
+        {
+          'hrsh7th/nvim-cmp',
+          config = function()
+            local cmp = require('cmp')
+            local cmp_action = require('lsp-zero').cmp_action()
+
+            cmp.setup({
+              mapping = {
+                ['<Tab>'] = cmp_action.tab_complete(),
+                ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+              },
+            })
+          end
+        },
         'hrsh7th/cmp-nvim-lsp',
         'L3MON4D3/LuaSnip',
       },
