95c1caf5f97111fe6cc4f44e378dc2be5320ec8b
Jason Wang <jawang35@gmail.com>
2023-10-14 15:47:56 -0700

    Preserve brew binary

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,7 +27,9 @@ if [ "$(uname -s)" == Darwin ]; then
     # Cloudflare/APNIC DNS
     networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001 || true
 
-    brew update && brew bundle --file="${HOME}/.config/brew/Brewfile"
+    if command -v brew > /dev/null 2>&1; then
+        brew update && brew bundle --file="${HOME}/.config/brew/Brewfile"
+    fi
 fi
 
 # shellcheck source=.bash_profile
