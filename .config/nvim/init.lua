0fa3e0574df3ea36f95256e1c17d11c4a065144e
Jason Wang <jawang35@gmail.com>
2023-11-28 21:01:51 -0800

    Use pylsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -148,24 +148,24 @@ if vim.loop.fs_stat(lazy_path) then
         'L3MON4D3/LuaSnip',
       },
       config = function()
-        local lsp = require('lsp-zero').preset({})
+        local lsp_zero = require('lsp-zero').preset({})
 
-        lsp.on_attach(function(client, bufnr)
-          lsp.default_keymaps({buffer = bufnr})
+        lsp_zero.on_attach(function(client, bufnr)
+          lsp_zero.default_keymaps({buffer = bufnr})
         end)
 
-        lsp.ensure_installed({
+        lsp_zero.ensure_installed({
           'eslint',
           'lua_ls',
-          'pyright',
+          'pylsp',
           'terraformls',
           'tflint',
           'tsserver',
         })
 
-        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+        require('lspconfig').lua_ls.setup(lsp_zero.nvim_lua_ls())
 
-        lsp.setup()
+        lsp_zero.setup()
       end
     },
     {
